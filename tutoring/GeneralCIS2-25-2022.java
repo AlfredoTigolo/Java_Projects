cbe43fbb63c4534a4712cf238fc46b8d1e8725f2
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2022-02-25 16:34:32 -0800

    Update GeneralCIS2-25-2022.java

--- tutoring/GeneralCIS2-25-2022.java
+++ tutoring/GeneralCIS2-25-2022.java
@@ -6,10 +6,12 @@ public class helloworld
     public static void main (String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int[] listInt = new int[50];        
-        int[] rank = new int[50];
+        int[] listInt = new int[51];
+        int[] rankInt = new int[51];
+        int[] rank = new int[51];
 
         int increment = 0;
+        int rankkey = 0;
 
         //System.out.println("hello world");
 
@@ -20,11 +22,20 @@ public class helloworld
             if (x == 99)
                 break;
             listInt [increment] = x;
-            System.out.println(Arrays.toString(listInt));
+            System.out.println(Arrays.toString(listInt));            
+            //for ( int j = 0; j < listInt.length - 1 ; j++)
+            for ( int j = 0; j < increment - 1 ; j++)
+            {                
+                if (x > listInt[j])
+                rankkey++;
+            }
+            System.out.print("rank " + rankkey);
+            rankInt[rankkey] = x;            
+            rankkey = 0;
             //System.out.println(Arrays.deepToString(listInt));
             increment++;
         } while (true);
+
+        System.out.println(Arrays.toString(rankInt));
     
     }
-
-}
