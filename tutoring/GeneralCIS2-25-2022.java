f5dc2288e48359f2262e198589f5e5fd6da5c8a5
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2022-02-25 16:07:01 -0800

    Update GeneralCIS2-25-2022.java

--- tutoring/GeneralCIS2-25-2022.java
+++ tutoring/GeneralCIS2-25-2022.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class helloworld
 {
     public static void main (String[] args)
     {
         Scanner in = new Scanner(System.in);
-        int[] listInt;
+        int[] listInt = new int[50];        
+        int[] rank = new int[50];
+
+        int increment = 0;
 
         //System.out.println("hello world");
 
         do {
             System.out.println("Enter number");
             String i = in.next();
-            int x = Integer.parseInt(i);
+            int x = Integer.parseInt(i);            
             if (x == 99)
                 break;
-            
+            listInt [increment] = x;
+            System.out.println(Arrays.toString(listInt));
+            //System.out.println(Arrays.deepToString(listInt));
+            increment++;
         } while (true);
     
     }
