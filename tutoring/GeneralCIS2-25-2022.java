a41867ee11a1703fa0089d89d3a01c229f1b854e
AlfredoTigolo <45888193+AlfredoTigolo@users.noreply.github.com>
2022-02-25 16:45:02 -0800

    Update GeneralCIS2-25-2022.java

--- tutoring/GeneralCIS2-25-2022.java
+++ tutoring/GeneralCIS2-25-2022.java
@@ -24,11 +24,15 @@ public class helloworld
             if (x == 99)
                 break;
             listInt [increment] = x;
-            System.out.println(Arrays.toString(listInt));            
+            System.out.println(Arrays.toString(listInt));   
+            
             //for ( int j = 0; j < listInt.length - 1 ; j++)
+            // j is the start of the for loop
+            // increment is the size of number input you entered
+            // rankkey gives the range of first, second, third, 4th, etc place
             for ( int j = 0; j < increment; j++)
             {                
-                if (x > listInt[j])
+                if (x > listInt[j]) // reverse the sign to less then to put in order from largest to smallest 2342, 2321, 46 are example numbers
                 rankkey++;
             }
             System.out.print("rank " + rankkey);
